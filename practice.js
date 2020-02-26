1e147aba31ab68e28ea7e073341a00580591b098
Li Jung Huang <lijunghuang@Lis-MacBook-Pro.local>
2020-02-26 15:27:42 -0700

    didntdoextra

--- practice.js
+++ practice.js
@@ -72,7 +72,7 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 function reversedLooper(letters){
-  for(let i = letters.length-1; i >= 0; i){
+  for(let i = letters.length-1; i >= 0; i--){
     alert(letters[i])
   }
 }
@@ -90,10 +90,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  let evennum = []
 
-
-
-
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i]%2 === 0){
+      evennum.push(nums[i])}
+    } return evennum
+} 
 
 
 
